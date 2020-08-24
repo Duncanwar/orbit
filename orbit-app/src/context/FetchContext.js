44ac6c65e5210937b3a9def4a911590d7c5b65ed
Duncanwar <semunda9@gmail.com>
2020-08-24 16:40:04 +0200

    auth

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,14 +1,26 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 import axios from 'axios';
+import { AuthContext } from './AuthContext';
 
 const FetchContext = createContext();
 const { Provider } = FetchContext;
 
 const FetchProvider = ({ children }) => {
+  const authContext = useContext(AuthContext);
+
   const authAxios = axios.create({
     baseURL: process.env.REACT_APP_API_URL
   });
 
+  authAxios.interceptors.request.use(
+    config => {
+      config.headers.Authorization = `Bearer ${authContext.authState.token}`;
+      return config;
+    }, error => {
+      console.log(error)
+      return Promise.reject(error)
+    }
+  )
   return (
     <Provider
       value={{
